a35e4a2d21032686136a3d6579d2357506e8a644
bely87 <bely87@yandex.ru>
2015-06-22 12:47:39 +0300

    Delete mysql.php

--- php/dbhost-localhost/dbuser-mysql-./dbpassword-mysql123-./dbname-asterisk-./mysql.php
+++
@@ -1,9 +0,0 @@
-<?php
-$host = "localhost";
-$user = "mysql";
-$pass = "mysql";
-$db   = "addon";
-$connect = @mysql_connect($host, $user, $pass);
-mysql_query('SET NAMES utf8', $connect);
-mysql_select_db($db, $connect);
-?>
